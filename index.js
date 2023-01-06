dfa2ec3a13c4db859cfcded08da624c2748f1844
sharad khambe <khambe.sharad@gmail.com>
2023-01-06 14:02:00 +0530

    First Version of profile CLI

---
+++ index.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+console.log(`
+Sharad Khambe 
+
+AWS cloud solution designer, Learning to become awesome AWS serverless developer as I believe serverless makes
+your life easy and manageble. 
+
+`)

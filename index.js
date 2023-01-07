240e0f529afeada5a0e73cf8ce1eb56aec7a68cb
sharad khambe <khambe.sharad@gmail.com>
2023-01-07 09:46:43 +0530

    Add clear console feature in index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 #! /usr/bin/env node
+
+//const clearConsole = require('clear-any-console');
+
+import clearConsole from 'clear-any-console';
+
+clearConsole();
+//console.clear();
+
 console.log(`
-Sharad Khambe 
+Sharad Khambe - Hello!
 
 AWS cloud solution designer, Learning to become awesome AWS serverless developer as I believe serverless makes
 your life easy and fun!!!. 
-`)
+`);
